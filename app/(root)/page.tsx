64c1ddbf5f636c8e9f5757ee630932fe551888fc
sharmatushar14 <sharmatusharofficial14@gmail.com>
2024-03-16 13:32:55 +0530

    Added User Authentication and Management

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { UserButton } from '@clerk/nextjs';
 
 // Define your React component
 const HomePage = () => {
   return (
     <div>
     Home
+    <UserButton afterSignOutUrl='/'/>
     </div>
   );
 };
