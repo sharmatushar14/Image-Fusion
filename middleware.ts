456036afe902300c1349ded85d5cea9d1915cdab
sharmatushar14 <sharmatusharofficial14@gmail.com>
2024-03-16 17:44:48 +0530

    Added SideBar with all media responsive

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({});
  
 export const config = {
+  publicRoutes: ["/"],
   // Protects all routes, including api/trpc.
   // See https://clerk.com/docs/references/nextjs/auth-middleware
   // for more information about configuring your Middleware
