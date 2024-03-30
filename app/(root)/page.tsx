44011a40febc707b3b3ed920ea4b126c3b51a83b
sharmatushar14 <sharmatusharofficial14@gmail.com>
2024-03-30 18:29:23 +0530

    Added the Pagination and Searching Function for HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import { Collection } from "@/components/shared/Collection"
 import { navLinks } from "@/constants"
+import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -6,11 +8,13 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
+  const images = await getAllImages({ page, searchQuery})
+
   return (
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with Imaginify
+          Unleash Your Creative Vision with Fusion
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
@@ -26,6 +30,15 @@ const Home = async ({ searchParams }: SearchParamProps) => {
             </Link>
           ))}
         </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   )
